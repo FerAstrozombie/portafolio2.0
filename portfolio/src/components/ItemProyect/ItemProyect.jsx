4910175d84fb8c5401fe07340550d6a12b2eec3d
flair <fernando_punk77@hotmail.com>
2024-10-10 16:51:52 -0300

    colocando proyectos

---
+++ portfolio/src/components/ItemProyect/ItemProyect.jsx
@@ -0,0 +1,25 @@
+import "./styles.css"
+import Item from "../Item/Item"
+
+const ItemProyect = ({lista}) => {
+    return(
+        (
+        <>
+            <h2 className="cursoTitulo">Proyectos</h2>
+        <div className="flexProyectos">
+            {
+                lista.map((proyecto, nombre) => (
+                <Item 
+                    key={nombre}
+                    nombre={proyecto.nombre}
+                    gif={proyecto.gif}
+                    link={proyecto.link}
+                    descripcion={proyecto.descripcion}
+                />))
+            }
+        </div>
+        </>
+    ))
+}
+
+export default ItemProyect
--- portfolio/src/components/Proyectos/Proyectos.jsx
+++ portfolio/src/components/Proyectos/Proyectos.jsx
@@ -1,9 +1,20 @@
 import "./styles.css"
+import { useEffect, useState } from "react"
+import data from "../mockData"
+import ItemProyect from "../ItemProyect/ItemProyect"
 
 const Proyectos = () => {
 
+    const [proyectList, setProyectList] = useState([]);
+    useEffect (() => {
+        setProyectList(data);
+    },[])
+    const getProyects = new Promise((resolve) =>{
+        resolve(data);
+    })
+
     return (
-        <div></div>
+        <ItemProyect lista = {proyectList} />
     )
 }
 
